ebd96a5ea0da66b9cbed9f44cfdf497e51e97548
Rajoza123 <ozaraj76@gmail.com>
2025-12-14 01:33:17 +0530

    refactor(auth): fix create method to correctly return saved user instance

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,8 +6,9 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = self.get_serializer().instance
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
 
         return Response({
             "id": user.id,

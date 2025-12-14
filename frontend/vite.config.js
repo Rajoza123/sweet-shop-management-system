0df52b96b6cbd5611abe23858d5a753949cc887f
Rajoza123 <ozaraj76@gmail.com>
2025-12-14 20:14:10 +0530

    frontend setup upgraded

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.js",
+  },
+});
--- frontend/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globals: true,          // enables describe, test, expect
-    environment: "jsdom",
-    setupFiles: "./src/testSetup.js",
-  },
-});

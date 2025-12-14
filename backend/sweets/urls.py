bf88791cca7e89f976e54eba757161ed4b40828e
Rajoza123 <ozaraj76@gmail.com>
2025-12-14 15:08:13 +0530

    feat(sweets): implement sweet list API with filtering to satisfy tests

--- backend/sweets/urls.py
+++ backend/sweets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SweetCreateView
+from .views import SweetCreateView , SweetListView
 
 urlpatterns = [
     path("create/", SweetCreateView.as_view(), name="sweet-create"),
+    path("", SweetListView.as_view(), name="sweet-list"),
 ]
--- backend/sweets/views.py
+++ backend/sweets/views.py
@@ -7,3 +7,27 @@ class SweetCreateView(generics.CreateAPIView):
     queryset = Sweet.objects.all()
     serializer_class = SweetSerializer
     permission_classes = [IsAdminUser]
+class SweetListView(generics.ListAPIView):
+    serializer_class = SweetSerializer
+
+    def get_queryset(self):
+        queryset = Sweet.objects.all()
+
+        category = self.request.query_params.get("category")
+        min_price = self.request.query_params.get("min_price")
+        max_price = self.request.query_params.get("max_price")
+        name = self.request.query_params.get("name")
+
+        if category:
+            queryset = queryset.filter(category=category)
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
